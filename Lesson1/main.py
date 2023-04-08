5c3ed4f7059a040d03febeca71e7358fdafcb01f
NikolayKartaev <kartaevgeek@gmail.com>
2023-04-08 06:46:04 +0300

    Lesson1

---
+++ Lesson1/main.py
@@ -0,0 +1,71 @@
+# n = 1
+# print(n)
+# print(type(n))
+# print()
+
+# n = 2.5
+# print(n)
+# print(type(n))
+# print()
+
+# n = "'example1'"
+# print(n)
+# print(type(n))
+# print()
+
+# n = '"example2"'
+# print(n)
+# print(type(n))
+# print()
+
+# n = '\'example3\''
+# print(n)
+# print(type(n))
+# print()
+
+# a = 5
+# b = 10.50
+# c = 'hello'
+# print (f"{a} - {b} - {c}")
+# print ("{} - {} - {}".format(a,b,c))
+
+
+
+# print("Введите число a:")
+# a = input()
+# b = input("Введите число b: ")
+# c = 5.2
+# d = 5
+# print(f"a = {a}")
+# print(type(a))
+
+# print(f"b = {b}")
+# print(type(b))
+
+# print(f"c = {c}")
+# print(type(c))
+
+# c = int(c)
+# print(type(c))
+
+# print(f"d = {d}")
+# print(type(d))
+
+
+
+# print("Введите число a:")
+# a = int(input())
+# b = int(input("Введите число b: "))
+# print(a, "+", b, "=", a+b)
+
+
+a = 5.542434345
+b = 4.55322245
+print(round(a*b, 4))
+
+
+
+
+
+
+
