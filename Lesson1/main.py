058867e2ab1bcc1e85a3bd21fb967b2ab13e4da7
NikolayKartaev <kartaevgeek@gmail.com>
2023-04-08 06:50:47 +0300

    Lesson1 Laptop

---
+++ Lesson1/main.py
@@ -0,0 +1,41 @@
+# a = 5
+# b = 10
+# c = 20.5
+# print(f"{a} + {b} + {c} = {a+b+c}")
+# print("{} + {} + {} = {}".format(a, b, c, a+b+c))
+
+# d = input()
+# f = input("Введите число")
+
+# r = 15
+# print(r+5)
+# print(type(r))
+
+# r = str(r)
+# print(r+"5")
+# print(type(r))
+
+
+
+# n = int(input())
+# flag = True
+# i = 2
+
+# while flag:
+#     if n % i == 0:
+#         flag = False
+#         print(i)
+#     elif i > n//2:
+#         print(n)
+#         flag = False
+#     i += 1
+
+
+# for i in range(5):  
+#     line="" 
+#     for j in range(5):
+#         line+="*"
+#     print(line)
+    
+text = '123456789'
+print(text[0::3])
