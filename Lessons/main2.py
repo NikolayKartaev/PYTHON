d9b6565ec088d77afb9f41fc0d4fd2e1f6c20bcf
NikolayKartaev <kartaevgeek@gmail.com>
2023-04-18 06:48:48 +0300

    Seminar3

--- Lessons/main2.py
+++ Lessons/main2.py
@@ -1,84 +1,214 @@
 # СПИСКИ
-
 # Создание нового пустого списка
 def CreateList():
-    list_1 = []  
+    list_1 = []
     list_2 = list()
 
+
  # Создание списка со значениями
-list_3 = [1, 2, 3, 4, 5] 
+list_3 = [1, 2, 3, 4, 5]
 
 # Вывод списка без скобочек и запятых - открытие списка
-def PrintList():
-    print(*list_3)  
 
+
+def PrintList():
+    print(*list_3)
 
 # ФУНКЦИИ СПИСКОВ
-
 # Добавление элемента в конец списка
+
+
 def Append():
-    list_3.append(6)  
+    list_3.append(6)
     print(*list_3)
 
 # Добавление элемента через цикл
+
+
 def AppendLoop():
     list_3_1 = list()
-    for i in range(10): 
+    for i in range(10):
         list_3_1.append(i)
         print(list_3_1)
-    
+
 # Удаление элемента с конца (.pop удаляет элемент и возвращает его )
+
+
 def Pop():
     list_3.append(6)
     print(list_3)
-    print(list_3.pop()) # Вывод значения удаленного элемента
+    print(list_3.pop())  # Вывод значения удаленного элемента
     print(list_3)
-    a = list_3.pop() # Возвращает значение удаленного элемента в переменную
+    a = list_3.pop()  # Возвращает значение удаленного элемента в переменную
     print(f"a = {a}")
     print(list_3)
-    print(list_3.pop(0)) # Удаление элемента с индексом
+    print(list_3.pop(0))  # Удаление элемента с индексом
     print(list_3)
 
 # Вставляем элемент на указанный индекс (нужную позицию)
+
+
 def Insert():
     print(list_3)
-    list_3.insert(2,10)
+    list_3.insert(2, 10)
     print(list_3)
 
+def shift(list, steps): # Сделать смещение списка ну указанное количество элементов
+    if steps < 0:
+        steps = abs(steps)
+        for i in range(steps):
+            print(list)
+            list.append(list.pop(0))
+             
+    else:
+        for i in range(steps):
+            print(list)
+            list.insert(0, list.pop())
+
+
 # Срезы списков
-print(list_3[-2:])
-print(list_3[::3])
 
 
-## КОРТЕЖ - неизменяемый список (занимает мало памяти, работает быстрее)
-
-# Создание нового котрежа
-def CreateTuple():
-    t = ()         # пустой кортеж
-    t = (1,2,)     # кортеж со значениями
-    
+def PrintList_2():
+    print(list_3[-2:])
+    print(list_3[::3])
 
 
+# КОРТЕЖ - неизменяемый список (занимает мало памяти, работает быстрее)
+
+# Создание нового кортежа
+t = ()         # пустой кортеж
+t1 = (1,)     # кортеж со значениями
 
 
+def ListTuple():
+    v = [1, 2, 3]
+    print(v)
+    print(type(v))
+
+    v = tuple(v)
+    print(v)
+    print(type(v))
 
 
+# Множественное присваивание
+a, b = 1, 2
+a = b = 1
 
 
-# LIST COMPREHENSION - ГЕНЕРАТОР СПИСКА
-def Comprehension ():
-    N = int(input())
-    list_4 = [a for a in range(1, N) if a % 2 == 0]
-    list_5 = [(a, a**2) for a in range(1, N) if a % 3 == 0]
+def Prisv():
+    v = [5, 10, 15]
+    v = tuple(v)
+    a, b, c = v
+    print(a, b, c)  # Печать трех переменных, равных элементам кортежа
+    print(*v)  # Вывод кортежа без скобочек и запятых - распаковка кортежа
 
-    print(list_4)
-    print(list_5)
+    for i in v:  # Вывод элементов списка/кортежа
+        print(i, end=" ")
+
+    print()
+
+    for i in range(len(v)):  # Вывод значений с индексами списка/кортежа
+        print(v[i], end=" ")
 
 
+# СЛОВАРИ - неупорядоченные коллекции произвольных объемов с доступом по ключу (строка, число)
+
+# Создание нового словаря
+d = dict()
+d = {}
+d = {"key": "value"}
 
 
-# Множества
+def Dictionary():
+    d['key1'] = 'data1'  # Добавление значения в словарь по ключу (строка)
+    print(d)
+    d['key2'] = 'data2'  # Добавление значения в словарь по ключу (строка)
+    print(d)
+
+    print(d['key1'])  # Печать значения по ключу
+    print(d['key2'])  # Печать значения по ключу
+
+    d['key2'] = 'data3'  # Перезаписать значения по ключу
+    d[123] = 100500  # Добавление значения в словарь по ключу (число)
+
+    del d['key1']  # Удаление значения по ключу
+    print(d)
+
+    for i in d:
+        print(i)
+
+    for i in d:
+        print(f"{i}: {d[i]}")
+        print("{}: {}".format(i, d[i]))
+
+    print(d.items())
+
+def Dictionary2():
+    anketa = dict(Имя = input('Ваше имя: '),
+                  Возраст = input('Ваш возраст: '),
+                  Хобби = input('Ваше хобби: '),
+                  Животное = input('Ваше любимое животное: '))
+    print()
+    for key, value in anketa.items():
+                print("{}: {}".format(key,value))
+
+Dictionary2()
+
+def Dictionary3():
+    dictionary = {}
+    dictionary['name'] = input("Введите свое имя: ")
+    dictionary['age'] = input("Введите свой возраст: ")
+    dictionary['hobby'] = input("Введите свое хобби: ")
+    dictionary['favorite_animal'] = input("Введите свое любимое животное: ")
+
+    for k, v in dictionary.items():
+        print(f"{k}: {v}")
+
+Dictionary3()
+
+# МНОЖЕСТВА - уникальные неупорядоченные значения
+
+
+q = set()  # Создание множества
+colors = {'red', 'green', 'blue'}  # Создание множества со значениями
+f = frozenset(colors)  # Заморожденное множество
+
+colors.add('red')  # Добавление значения (не уникаольное, не добавит)
+colors.add('grey')  # Добавление значения (уникальное, добавит)
+
+colors.remove('blue')  # Удаление значения
+# Удаление значения (если не может удалить, то пропускает)
+colors.discard('blue')
+colors.clear()  # Удаление всех значений множества
+
+# Операции со множествами
 a = {1, 2, 3, 5, 8}
 b = {2, 5, 8, 13, 21}
 
+c = a.copy()  # Копирование значений множества
+u = a.union(b)  # Объединение множеств
+i = a.intersection(b)  # Пересечение множеств
+d1 = a.difference(b)  # Разность множеств a и b
+d2 = b.difference(a)  # Разность множеств b и a
 q = a.union(b).difference(a.intersection(b))
+
+
+# LIST COMPREHENSION - ГЕНЕРАТОР СПИСКА
+
+def Comprehension():
+    N = int(input())
+    list_4 = ["abc" for i in range(5)]
+    list_5 = [i for i in range(1, 101) if i % 2 == 0]
+    list_6 = [a for a in range(1, N) if a % 2 == 0]
+    list_7 = [(a, a) for a in range(1, N)]
+    list_8 = [(a, a**2) for a in range(1, N) if a % 3 == 0]
+    list_9 = [i*2 for i in range(10)]
+    list_10 = [i*2 for i in range(10)if i % 2 == 0]
+
+    print(list_9)
+    print(list_10)
+
+
+# Indented block - блок с отступом
+# IndentationErroe (Ошибка отступов)
--- Seminars/main.py
+++ Seminars/main.py
@@ -1,7 +1,12 @@
+import string
+import random
+
 # Задача 0
+
+
 def Power():
     a = int(input("Введите число a: "))
-    b=a*a
+    b = a*a
     print(f"Квадрат {a} = {b}")
 
 # Задача 1
@@ -61,8 +66,7 @@ def Power():
 # numbers = [5,-4,8,9,-9,4,7,0,1,-5]
 # for i in numbers:
 #     print(i)
-    
-    
+
 
 def CheckEvenOrOdd(value):
     if value % 2 == 0:
@@ -106,7 +110,7 @@ def FindBoolValue(a, b):
         return 0
 
 
-#Задача 1. Выведите таблицу истинности для выражения ¬ X ∨ Y.
+# Задача 1. Выведите таблицу истинности для выражения ¬ X ∨ Y.
 def Task1():
     print('X Y R')
     for x in range(2):
@@ -119,6 +123,7 @@ def Task1():
 # y = int(input("Введите Y: "))
 # FindBoolValue(x,y)
 
+
 def Task1_1():
     print()
     print('X | Y | ¬ X ∨ Y')
@@ -127,17 +132,18 @@ def Task1_1():
             print(f"{x} | {y} |    {int(not x or y)}")
 
 
-#Задача 2. Напишите программу, в которой пользователь будет задавать две строки, а программа - определять количество вхождений одной строки в другую.
+# Задача 2. Напишите программу, в которой пользователь будет задавать две строки, а программа - определять количество вхождений одной строки в другую.
 def Task2(a, b):
     count = 0
     for i in range(len(b)-len(a)+1):
         if a == b[i:i+len(a)]:
-            count+= 1
-    print (count)
+            count += 1
+    print(count)
 
 # str1= input("Введите строку 1: ")
 # str2= input("Введите строку 2: ")
-#Task2(str1, str2)
+# Task2(str1, str2)
+
 
 def Task2_1():
     string = input("Введите строку: ")
@@ -149,7 +155,7 @@ def Task2_1():
     print(count)
 
 
-#Вывод по элементам
+# Вывод по элементам
 # phrase = input("Введите фразу: ")
 # for el in phrase:
 #     print(el, end='')
@@ -160,29 +166,29 @@ def Task2_1():
 #     print(phrase[i], end='')
 
 
-#Вывод дробного числа с указанием количества знаков после запятой без округления
+# Вывод дробного числа с указанием количества знаков после запятой без округления
 def float():
     fl = 2.715415864
     print(f"{fl:.6f}")
 
 
-#Задача 3. Дано число N. Заполните список длиной N элементами 1, -3, 9, -27, 81, -243...
+# Задача 3. Дано число N. Заполните список длиной N элементами 1, -3, 9, -27, 81, -243...
 def Task3_1():
     N = int(input())
-    array=[]
+    array = []
     for i in range(N):
         array.append((-3)**i)
-        if i==N-1:
-            print (array)
-        
+        if i == N-1:
+            print(array)
+
+
 def Task3_2():
     N = int(input())
     for i in range(N):
-            print(f"{(-3)**i}", end=' ')
+        print(f"{(-3)**i}", end=' ')
 
 
-
-#Задача 4. Найдите все числа до 10000, у который количество делителей равно 10.
+# Задача 4. Найдите все числа до 10000, у который количество делителей равно 10.
 
 def Task0_3(number):
     count = 0
@@ -191,45 +197,220 @@ def Task0_3(number):
             count += 1
     return count
 
+
 def Task4():
-    countnumbers=0
-    for number in range(1,10001):
-        if Task0_3(number)==10:
-            countnumbers+=1
+    countnumbers = 0
+    for number in range(1, 10001):
+        if Task0_3(number) == 10:
+            countnumbers += 1
             print(f"{number}\t", end='')
     print()
     print(f"Количество чисел с делителем 10 равно: {countnumbers}")
 
 
 def Task4_1():
-    countnumbers=0
-    for numbers in range(1,10001):
-        countdivs=0
-        for i in range (1, numbers+1):
-            if numbers%i==0:
-                countdivs+=1        
-        if countdivs==10:
-            countnumbers+=1
+    countnumbers = 0
+    for numbers in range(1, 10001):
+        countdivs = 0
+        for i in range(1, numbers+1):
+            if numbers % i == 0:
+                countdivs += 1
+        if countdivs == 10:
+            countnumbers += 1
             print(f"{numbers}\t", end='')
     print()
     print(countnumbers)
 
 
-def shift(lst, steps):
+def shift(list, steps):  # Сделать смещение списка ну указанное количество элементов
     if steps < 0:
         steps = abs(steps)
         for i in range(steps):
-            lst.append(lst.pop(0))
+            print(list)
+            list.append(list.pop(0))
+
     else:
         for i in range(steps):
-            lst.insert(0, lst.pop())
-  
-nums = [4, 5, 6, 7, 8, 9, 0]
-print(nums)
- 
-shift(nums, -2)
-print(nums)
- 
-shift(nums, 3)
-print(nums)
+            print(list)
+            list.insert(0, list.pop())
 
+
+# SEMINAR_3
+
+def TaskS3_1():
+    length = 7
+    # numbers = [0]*length
+    # for i in range(length):
+    #     numbers[i] = random.randint(0,10)          создание списка через цикл
+    # print(numbers)
+
+    length = int(input("Введите количество элементов списка: "))
+    numbers = [int(input("Введите элемент списка: "))
+               for _ in range(length)]  # создание списка через генератор списков
+    print(numbers)
+    print(sum(numbers))
+    if sum(numbers) % 2 == 0:
+        print("Сумма всех элементов четная")
+    else:
+        print("Сумма всех элементов нечетная")
+
+    # укоротил код!!!
+    numbers = [int(input("Введите элемент списка: "))for _ in range(
+        int(input("Введите количество элементов списка: ")))]
+    print(numbers)
+    print("Сумма всех элементов четная" if sum(numbers) %
+          2 == 0 else "Сумма всех элементов нечетная")
+    # укоротил код!!!
+
+    # numbers = (random.randint(0,10)for i in range(length)) # создание генератора
+    # numbers=list(numbers)                         # создание списка из генератора
+    # print(numbers)
+    # print(type(numbers))
+
+
+# Задача 1. В списке хранятся сведения о количестве осадков, выпавших за каждый день июня. Определите в какой период выпало больше осадков:
+# в первой или второй половине июня.
+
+def TaskS3_2():
+    sumFirsthalf = sumSecondhalf = 0
+    sumFirsthalf, sumSecondhalf = 0, 0
+
+    list = [random.randint(0, 25)for _ in range(30)]
+
+    # for i in range(len(list)/2):
+    #     sumFirsthalf+=list[i]            - подсчет сразу двух переменных в одном цикле
+    #     sumSecondhalf+=list[i+15]
+
+    # print(list)
+    # if sumFirsthalf > sumSecondhalf:
+    #     print(f"В первой половине июня выпало больше осадков")
+    # else:
+    #     print(f"В второй половине июня выпало больше осадков")
+
+    sumFirsthalf = sum(list[:len(list)//2])
+    sumSecondhalf = sum(list[len(list)//2:])
+    print(list)
+    print(sumFirsthalf)
+    print(sumSecondhalf)
+    print("В первой половине июня выпало больше осадков" if sumFirsthalf >
+          sumSecondhalf else "Во второй половине июня выпало больше осадков")
+
+
+# Задача 2.
+# заполнить анкету, последовательно вводя в программу:
+# - имя;
+# - возраст;
+# - хобби;
+# - любимое животное.
+# После окончания ввода, выводится заполненная анкета.
+def TaskS3_3():
+    anketa = dict(Имя=input('Ваше имя: '),
+                  Возраст=input('Ваш возраст: '),
+                  Хобби=input('Ваше хобби: '),
+                  Животное=input('Ваше любимое животное: '))
+    print()
+
+    for key, value in anketa.items():
+        print("{0}: {1}".format(key, value))
+    print(anketa)
+
+
+def Dictionary3():
+    dictionary = {}
+    dictionary['name'] = input("Введите свое имя: ")
+    dictionary['age'] = input("Введите свой возраст: ")
+    dictionary['hobby'] = input("Введите свое хобби: ")
+    dictionary['favorite_animal'] = input("Введите свое любимое животное: ")
+
+    for (k, v) in dictionary.items():
+        print(k+":", v)
+
+
+# Задача 3. Напишите скрипт генератора паролей заданной длины, состоящих из латинских букв и чисел.
+
+def TaskS3_4():
+
+    symbols = string.ascii_letters + string.digits + string.punctuation
+
+    password = [symbols[random.randint(0, len(symbols))] for _ in range(
+        int(input("Введите длину пароля: ")))]
+    print(*password, end='')
+
+
+# Задача 4. Ручка стоит – 5 рублей, карандаш – 3 рубля, ластик – 4 рубля.
+# Кассир последовательно вводит в программу
+# позиции из чека «ручка», «карандаш», «ластик».
+# Ввод заканчивается, когда введено слово «стоп». Определите сумму чека.
+
+def TaskS3_5():
+    prices = {"ручка": 5, "карандаш": 3, "ластик": 4} # или prices = dict(ручка = 5, карандаш = 3, ластик = 4) 
+    check = 0
+    flag = True
+    while flag:
+        goods = input("Введите наименование товара:\n").lower()
+        if goods == "стоп":
+            flag = False
+        elif goods in prices:
+            check += prices[goods] # Ищет в словаре prices по ключу введенного в переменную goods
+        else:
+            print("Такого товара нет. Введите корректное название")
+    print(f"Сумма чека: {check}")
+
+
+TaskS3_5()
+
+
+# СЕМИНАР 4
+
+# Создайте кортеж, заполненный случайными числами. Напишите метод, который заменяет элемент в кортеже по заданному индексу другим случайным числом.
+def change_element(numbers, index):
+    return numbers[:index] + (random.randint(1, 100), ) + numbers[index+1:]
+
+
+def TaskS4_2():
+    tuple1 = tuple(random.randint(1, 10) for _ in range(5))
+    index = 2
+    print(*tuple1)
+
+
+# index = 2
+# print(numbers)
+# numbers = change_element(numbers, index)
+# print(numbers)
+# print(type(numbers))
+
+
+# Задача 2
+
+# def TaskS4_3(x,y):
+#     return (x+y, x-y, x*y, x/y)
+
+# x=int(input())
+# y=int(input())
+# tuple2 = TaskS4_3(x,y)
+# print(*tuple2)
+
+# x = int(input())
+# y = int(input())
+# print(" {}, {}, {}, {}".format(x+y,x-y,x*y,x/y))
+
+# ЗАДАЧА 3
+# Сгенерируйте список случайных чисел от 1 до 20, состоящий из 10 элементов.
+# Удалите из списка дубликаты уже имеющихся элементов. Определите, сколько элементов было удалено.
+
+def TaskS4_4():
+    list1 = [random.randint(1, 20) for _ in range(10)]
+    set1 = set(list1)
+    list2 = list(set1)
+    print(f"Список на 10 элементов: {list1}")
+    print(f"Список без повторяющихся значений:{list2}")
+    print(f"Удалено элементов: {len(list1) - len(list2)}")
+
+
+def TaskS4_5():
+    A = set("Илья Федор Семен Олег Лев Антон Артем Боря Стас Марк Ян".split())
+    B = set("Илья Георгий Лев Демьян Антон Владислав Боря Стас Марк Влад".split())
+    C = set("Федор Георгий Олег Демьян Артем Елисей Боря Стас Влад".split())
+
+    result = A.intersection(B).intersection(C)
+    print(result)
