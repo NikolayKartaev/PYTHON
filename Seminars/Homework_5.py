05a2eb28086cac7b1b728e9af2cacc7ce3e1f782
NikolayKartaev <kartaevgeek@gmail.com>
2023-04-26 12:56:41 +0300

    Homework5

--- Seminars/Homework_5.py
+++ Seminars/Homework_5.py
@@ -5,8 +5,8 @@ from random import randint
 # 2, 3, 4, 6, 7, 8 -> 6, 7, 8
 
 def Task1():
-    print(list(filter(lambda x: x > 5, [randint(1, 10) for _ in range(10)])))
-
+    random_list=[randint(1, 10) for _ in range(10)]
+    print(f"\nИсходный список случайных чисел: {random_list}\nЭлементы больше 5 -> {list(filter(lambda x: x > 5, random_list))}\n")
 
 
 # Задача 2. Дан список случайных чисел. Создайте список, в который попадают числа, описывающие случайную возрастающую последовательность.
@@ -16,21 +16,22 @@ def Task1():
 
 def Task2():
     numbers=[randint(1,15) for _ in range(10)]
-    print (f"Исходный список случайных чисел:{numbers}")
+    print (f"\nИсходный список случайных чисел:{numbers}")
     
     checklist1=[]
     rand_index=randint(0,len(numbers)-2)
+    
+    while numbers[rand_index]>=max(numbers[rand_index:]):
+        rand_index=randint(0,len(numbers)-2)
+    
     checklist1.append(numbers[rand_index])
 
     for i in range (rand_index,len(numbers)):
         if numbers[i]>max(checklist1):
             checklist1.append(numbers[i]) 
             
-    print(f"Случайная возрастающая последовательность начиная с позиции {rand_index}: {checklist1}")
+    print(f"Случайная возрастающая последовательность начиная с позиции {rand_index}: {checklist1}\n")
     
-
-Task2()
-
 
 
 # Задача 3. Задайте список случайных чисел от 1 до 10. Посчитайте, сколько всего совпадающих элементов есть в списке. Удалите все повторяющиеся элементы.
@@ -40,7 +41,7 @@ Task2()
 
 def Task3():
     list1=[randint(1,15) for _ in range(10)]
-    print(f"Исходный список чисел: {list1}")
+    print(f"\nИсходный список чисел: {list1}")
 
     checklist1=[]
     checklist2=[]
@@ -52,6 +53,6 @@ def Task3():
             checklist2.append(list1[i])
 
     print(f"Список уникальных элементов: {checklist1}")
-    print(f"{len(checklist2)+len(set(checklist2))} элемента(ов) совпадают")
+    print(f"{len(checklist2)+len(set(checklist2))} элемента(ов) совпадают\n")
 
 
