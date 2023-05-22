bb1ee5c220c7e9859de1a601b739e9ca549115e8
NikolayKartaev <kartaevgeek@gmail.com>
2023-05-22 17:56:35 +0300

    Seminar2 Analyst

--- Seminars/main2.py
+++ Seminars/main2.py
@@ -1,5 +1,5 @@
 import math
-
+# СЕМИНАР 1. Аналитик. 18.05.2023
 def Zadacha1():
     n=700
     m=750
@@ -46,3 +46,69 @@ def Zadacha7():
         print("NO")
         
 
+# СЕМИНАР 2. Аналитик. 22.05.2023
+
+def ZadachaS2_1():
+    N=int(input("Введите число N: "))
+    while N<0:
+        N=int(input("Вы ввели неверное число.\nВведите неотрицательное число: "))
+    result=i=1
+    while i<=N:
+        result= result*i
+        i+=1   
+    print(f"{N}! = {result}")
+        
+
+def ZadachaS2_2():
+    A=int(input("Введите число A: "))
+    while A<=1:
+        A=int(input("Вы ввели неверное число.\nВведите число больше 1: "))
+    x0 = 0
+    x1 = 1
+    x = 0
+    i = 2
+    while x < A:
+        x = x0 + x1
+        x0 = x1
+        x1 = x
+        i += 1
+        if x > A:
+            i = 0
+
+    if i == 0:
+        print(-1)
+    else:
+        print(i)
+               
+               
+def ZadachaS2_3():   
+    N=int(input())
+    count=0
+    x=int(input())
+    while N>100 and N<1:
+        N=int(input("Вы ввели неверное число.\nВведите верное: "))
+    for i in range(N-1):
+        x2=int(input())
+        if x2>x:
+            count+=1
+        x=x2
+    print(count)
+             
+             
+def ZadachaS2_4():
+    N=int(input("Введите количество арбузов: "))
+    weight=int(input("Введите вес арбуза: "))
+    max=min=weight
+    for _ in range(N-1):
+        weight=int(input("Введите вес арбуза: "))
+        while weight>1000 and weight<1:
+            weight=int(input("Вы ввели неверное число.\nВведите верное: "))
+        
+        if weight>max:
+            max=weight
+        if weight<min:
+            min=weight
+            
+    print(max,min)
+        
+    
