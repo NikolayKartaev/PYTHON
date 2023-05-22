e3b64b99fe8ea422d1be090f4fb270e7d1fdd6d2
NikolayKartaev <kartaevgeek@gmail.com>
2023-05-22 18:34:54 +0300

    HW7

---
+++ Seminars/Homework_7.py
@@ -0,0 +1,29 @@
+# Задача 1. Создайте пользовательский аналог метода map().
+def Zadacha1():
+    def function_map(function, our_list):
+        return [function(x) for x in our_list]
+
+    list1=[int(input("Элементы списка: ")) for _ in range(int(input("Кол-во элементов списка: ")))]
+    print(f"Исходный список: {list1}")
+    print(f"Конечный список: {function_map(lambda x: x+5,list1)}") # проверяем работоспособность
+
+# Задача 2. Создайте декоратор, повторяющий функцию заданное количество раз.
+def Zadacha2():
+    def times(N):
+        def repeat(function):
+            def decorator():
+                for i in range(N):
+                    function()
+            return decorator
+        return repeat
+
+
+    @times(int(input("Cколько раз выводить? ")))
+    def greetings():
+        print("Hello",end='\t')
+        
+        
+    greetings()
+    
+# Задача 3. Добавьте в telegram-бота игру «Угадай числа». Бот загадывает число от 1 до 1000.
+# Когда игрок угадывает его, бот выводит количество сделанных ходов.
